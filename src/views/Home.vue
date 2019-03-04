1bc72a0db1d520e689640231cd5b489bdfe73a08
Andy Gray <andy@whatsthescore.co.uk>
2019-03-04 15:56:29 +0000

    no skin home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
         <div class="row pb-4">
             <div class="col">
                 <h1 class="mt-5">{{ $t('common.nifty_football') }}</h1>
-                <!--<img src="../assets/card.svg" style="max-height: 300px" class="mt-4"/>-->
+                <img src="../assets/no_skin.svg" style="max-height: 300px" class="mt-4"/>
                 <h3 class="mt-3">{{ $t('common.collect_trade_play') }}</h3>
             </div>
         </div>
