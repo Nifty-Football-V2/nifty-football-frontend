ce1a818bafa199fcd52c9ef56c195ab56eecf859
Andy Gray <andy@whatsthescore.co.uk>
2019-05-02 11:52:56 +0100

    buy proto

--- src/views/Buy.vue
+++ src/views/Buy.vue
@@ -19,37 +19,93 @@
                 </div>
             </div>
 
-            <div class="row pb-5 pt-5 bg-dark" v-if="buyState === 'idle'">
-                <div class="col-sm"></div>
-                <div class="col-6 offset-3 offset-sm-0 col-sm-2 text-center">
-                    <img src="../assets/packs.svg" @click="buyCard(3, true)"/>
-                </div>
-                <div class="col-sm"></div>
-            </div>
+            <!--<div class="row pb-5 pt-5 bg-dark" v-if="buyState === 'idle'">-->
+            <!--<div class="col-sm"></div>-->
+            <!--<div class="col-6 offset-3 offset-sm-0 col-sm-2 text-center">-->
+            <!--<img src="../assets/packs.svg" @click="buyCard(3, true)"/>-->
+            <!--</div>-->
+            <!--<div class="col-sm"></div>-->
+            <!--</div>-->
 
             <div class="row" v-if="buyState === 'idle'">
                 <div class="col-lg"></div>
-                <div class="col text-center" v-if="accountCredits > 0">
-                    <b-dropdown split @click="buyCard(accountCredits >= 3 ? 3 : accountCredits, true)"
-                                text="Use Credits" class="mt-5" variant="secondary" size="lg" :disabled="!packPrices">
-                        <b-dropdown-item href="#" @click="buyCard(1, true)" v-if="accountCredits >= 1">Buy 1 Card
-                        </b-dropdown-item>
-                        <b-dropdown-item href="#" @click="buyCard(3, true)" v-if="accountCredits >= 3">Buy 1 Pack
-                        </b-dropdown-item>
-                    </b-dropdown>
-
-                    <div class="mt-3"><span class="badge badge-secondary">{{accountCredits}}</span> credits</div>
-                </div>
                 <div class="col text-center">
+                    <div class="buy-container bg-light pb-5">
+                        <div class="ml-4 mr-4">
+                            <page-sub-title text="Standard" variant="orange"></page-sub-title>
+                            <div class="row m-2">
+                                <div class="col"></div>
+                                <div class="col-8 buy-button buy-button-active">Regular - 1 Pack</div>
+                                <div class="col"></div>
+                            </div>
+                            <div class="row m-2">
+                                <div class="col"></div>
+                                <div class="col-8 buy-button">Large - 2 Packs</div>
+                                <div class="col"></div>
+                            </div>
+                            <div class="row m-2">
+                                <div class="col"></div>
+                                <div class="col-8 buy-button">XXL - 3 Packs</div>
+                                <div class="col"></div>
+                            </div>
 
-                    <b-dropdown split @click="buyCard(3)" text="Buy Pack" class="mt-5" variant="secondary" size="lg"
-                                :disabled="!packPrices">
+                            <div class="row mt-5">
+                                <div class="col"></div>
+                                <div class="col price-window text-right pt-2 pb-2">0.03 ETH</div>
+                                <div class="col"></div>
+                            </div>
+
+                            <button class="btn btn-secondary mt-3">Purchase</button>
+                        </div>
+                    </div>
+
+                    <b-dropdown split @click="buyCard(3)" text="Buy Pack" class="mt-5" variant="secondary" size="lg" :disabled="!packPrices">
                         <b-dropdown-item href="#" @click="buyCard(3)">Buy 1 Pack</b-dropdown-item>
                         <b-dropdown-item href="#" @click="buyCard(6)">Buy 2 Packs</b-dropdown-item>
                         <b-dropdown-item href="#" @click="buyCard(9)">Buy 3 Packs</b-dropdown-item>
                     </b-dropdown>
 
-                    <div class="small">1 pack is 3 cards</div>
+                    <div v-if="accountCredits > 0" class="mt-3">
+                        <b-dropdown split @click="buyCard(accountCredits >= 3 ? 3 : accountCredits, true)"
+                                    text="Use Credits" class="mt-5" variant="secondary" size="lg" :disabled="!packPrices">
+                            <b-dropdown-item href="#" @click="buyCard(1, true)" v-if="accountCredits >= 1">Buy 1 Card
+                            </b-dropdown-item>
+                            <b-dropdown-item href="#" @click="buyCard(3, true)" v-if="accountCredits >= 3">Buy 1 Pack
+                            </b-dropdown-item>
+                        </b-dropdown>
+
+                        <div class="mt-3"><span class="badge badge-secondary">{{accountCredits}}</span> credits</div>
+                    </div>
+                </div>
+                <div class="col text-center">
+                    <div class="buy-container bg-light pb-5">
+                        <div class="ml-4 mr-4">
+                            <page-sub-title text="Elite" variant="blue"></page-sub-title>
+                            <div class="row m-2">
+                                <div class="col"></div>
+                                <div class="col-8 buy-button buy-button-active">Regular - 1 Pack</div>
+                                <div class="col"></div>
+                            </div>
+                            <div class="row m-2">
+                                <div class="col"></div>
+                                <div class="col-8 buy-button">Large - 2 Packs</div>
+                                <div class="col"></div>
+                            </div>
+                            <div class="row m-2">
+                                <div class="col"></div>
+                                <div class="col-8 buy-button">XXL - 3 Packs</div>
+                                <div class="col"></div>
+                            </div>
+
+                            <div class="row mt-5">
+                                <div class="col"></div>
+                                <div class="col price-window text-right pt-2 pb-2">0.03 ETH</div>
+                                <div class="col"></div>
+                            </div>
+
+                            <button class="btn btn-secondary mt-3">Purchase</button>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-lg"></div>
             </div>
@@ -73,9 +129,10 @@
     import BuyPlayerReveal from '../components/BuyPlayerReveal';
     import NiftyFootballHeader from '../components/NiftyFootballHeader';
     import PageTitle from '../components/PageTitle';
+    import PageSubTitle from '../components/PageSubTitle';
 
     export default {
-        components: {PageTitle, NiftyFootballHeader, BuyPlayerReveal, Loading, NetworkWeb3Banner},
+        components: {PageSubTitle, PageTitle, NiftyFootballHeader, BuyPlayerReveal, Loading, NetworkWeb3Banner},
         data () {
             return {
                 packPrices: {},
@@ -165,9 +222,30 @@
 </script>
 
 <style lang="scss" scoped>
+    @import "../colours";
+
     .btn-xxl {
         padding: 20px 26px;
         font-size: 35px;
         border-radius: 8px;
+    }
+
+    .buy-button {
+        font-family: 'CrackerJack', sans-serif;
+        text-transform: uppercase;
+        font-size: 1.1rem;
+        background-color: $lightgrey;
+        border: 2px solid $lightgrey;
+    }
+
+    .buy-button-active {
+        border: 2px solid $secondary !important;
+    }
+
+    .price-window {
+        background-color: $black;
+        color: $secondary;
+        font-family: 'CrackerJack', sans-serif;
+        font-size: 1.25rem;
     }
 </style>
