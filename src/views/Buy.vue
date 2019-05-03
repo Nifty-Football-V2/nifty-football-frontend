ecd215a83db0cf7c6173a25971de0150bbaa2e3f
Andy Gray <andy@whatsthescore.co.uk>
2019-05-03 16:18:30 +0100

    fade on switch back

--- src/views/Buy.vue
+++ src/views/Buy.vue
@@ -25,98 +25,100 @@
                 </div>
             </div>
 
-            <div class="row" v-if="buyState === 'idle'">
-                <div class="col-lg"></div>
-                <div class="col text-center">
-                    <div class="buy-container bg-light pb-5">
-                        <div class="ml-4 mr-4">
-                            <page-sub-title text="Regular" variant="orange"></page-sub-title>
+            <transition name="fade">
+                <div class="row" v-if="buyState === 'idle'">
+                    <div class="col-lg"></div>
+                    <div class="col text-center">
+                        <div class="buy-container bg-light pb-5">
+                            <div class="ml-4 mr-4">
+                                <page-sub-title text="Regular" variant="orange"></page-sub-title>
 
-                            <div class="row m-2">
-                                <div class="col"></div>
-                                <div class="col-8 buy-button" @click="setPackType('reg-1')" :class="{'buy-button-active': packType === 'reg-1'}">1 Pack</div>
-                                <div class="col"></div>
-                            </div>
-                            <div class="row m-2">
-                                <div class="col"></div>
-                                <div class="col-8 buy-button" @click="setPackType('reg-2')" :class="{'buy-button-active': packType === 'reg-2'}">2 Packs</div>
-                                <div class="col"></div>
-                            </div>
-                            <div class="row m-2">
-                                <div class="col"></div>
-                                <div class="col-8 buy-button" @click="setPackType('reg-3')" :class="{'buy-button-active': packType === 'reg-3'}">3 Packs</div>
-                                <div class="col"></div>
-                            </div>
+                                <div class="row m-2">
+                                    <div class="col"></div>
+                                    <div class="col-8 buy-button" @click="setPackType('reg-1')" :class="{'buy-button-active': packType === 'reg-1'}">1 Pack</div>
+                                    <div class="col"></div>
+                                </div>
+                                <div class="row m-2">
+                                    <div class="col"></div>
+                                    <div class="col-8 buy-button" @click="setPackType('reg-2')" :class="{'buy-button-active': packType === 'reg-2'}">2 Packs</div>
+                                    <div class="col"></div>
+                                </div>
+                                <div class="row m-2">
+                                    <div class="col"></div>
+                                    <div class="col-8 buy-button" @click="setPackType('reg-3')" :class="{'buy-button-active': packType === 'reg-3'}">3 Packs</div>
+                                    <div class="col"></div>
+                                </div>
 
-                            <div class="row mt-5">
-                                <div class="col-2"></div>
-                                <div class="col price-window text-right pt-2 pb-2">
-                                    &nbsp;
-                                    <transition name="slide-fade">
+                                <div class="row mt-5">
+                                    <div class="col-2"></div>
+                                    <div class="col price-window text-right pt-2 pb-2">
+                                        &nbsp;
+                                        <transition name="slide-fade">
                                         <span v-if="packType.startsWith('reg')">
                                             {{ price | toEth }} ETH
                                         </span>
-                                    </transition>
+                                        </transition>
+                                    </div>
+                                    <div class="col-2"></div>
                                 </div>
-                                <div class="col-2"></div>
-                            </div>
 
-                            <button class="btn btn-secondary mt-3" :disabled="packType.startsWith('elite')" @click="buyCard()">Purchase</button>
+                                <button class="btn btn-secondary mt-3" :disabled="packType.startsWith('elite')" @click="buyCard()">Purchase</button>
+                            </div>
+                        </div>
+
+                        <div v-if="accountCredits > 0" class="mt-3">
+                            <b-dropdown split @click="buyCard(accountCredits >= 3 ? 3 : accountCredits, true)"
+                                        text="Use Credits" class="mt-5" variant="secondary" :disabled="!packPrices">
+                                <b-dropdown-item href="#" @click="buyCard(true)" v-if="accountCredits >= 1">Buy 1 Card
+                                </b-dropdown-item>
+                                <b-dropdown-item href="#" @click="buyCard(true)" v-if="accountCredits >= 3">Buy 1 Pack
+                                </b-dropdown-item>
+                            </b-dropdown>
+
+                            <div class="mt-3"><span class="badge badge-secondary">{{accountCredits}}</span> credits</div>
                         </div>
                     </div>
+                    <div class="col text-center">
+                        <div class="buy-container bg-light pb-5">
+                            <div class="ml-4 mr-4">
+                                <page-sub-title text="Elite" variant="blue"></page-sub-title>
 
-                    <div v-if="accountCredits > 0" class="mt-3">
-                        <b-dropdown split @click="buyCard(accountCredits >= 3 ? 3 : accountCredits, true)"
-                                    text="Use Credits" class="mt-5" variant="secondary" :disabled="!packPrices">
-                            <b-dropdown-item href="#" @click="buyCard(true)" v-if="accountCredits >= 1">Buy 1 Card
-                            </b-dropdown-item>
-                            <b-dropdown-item href="#" @click="buyCard(true)" v-if="accountCredits >= 3">Buy 1 Pack
-                            </b-dropdown-item>
-                        </b-dropdown>
+                                <div class="row m-2">
+                                    <div class="col"></div>
+                                    <div class="col-8 buy-button" @click="setPackType('elite-1')" :class="{'buy-button-active': packType === 'elite-1'}">1 Pack</div>
+                                    <div class="col"></div>
+                                </div>
+                                <div class="row m-2">
+                                    <div class="col"></div>
+                                    <div class="col-8 buy-button" @click="setPackType('elite-2')" :class="{'buy-button-active': packType === 'elite-2'}">2 Packs</div>
+                                    <div class="col"></div>
+                                </div>
+                                <div class="row m-2">
+                                    <div class="col"></div>
+                                    <div class="col-8 buy-button" @click="setPackType('elite-3')" :class="{'buy-button-active': packType === 'elite-3'}">3 Packs</div>
+                                    <div class="col"></div>
+                                </div>
 
-                        <div class="mt-3"><span class="badge badge-secondary">{{accountCredits}}</span> credits</div>
-                    </div>
-                </div>
-                <div class="col text-center">
-                    <div class="buy-container bg-light pb-5">
-                        <div class="ml-4 mr-4">
-                            <page-sub-title text="Elite" variant="blue"></page-sub-title>
-
-                            <div class="row m-2">
-                                <div class="col"></div>
-                                <div class="col-8 buy-button" @click="setPackType('elite-1')" :class="{'buy-button-active': packType === 'elite-1'}">1 Pack</div>
-                                <div class="col"></div>
-                            </div>
-                            <div class="row m-2">
-                                <div class="col"></div>
-                                <div class="col-8 buy-button" @click="setPackType('elite-2')" :class="{'buy-button-active': packType === 'elite-2'}">2 Packs</div>
-                                <div class="col"></div>
-                            </div>
-                            <div class="row m-2">
-                                <div class="col"></div>
-                                <div class="col-8 buy-button" @click="setPackType('elite-3')" :class="{'buy-button-active': packType === 'elite-3'}">3 Packs</div>
-                                <div class="col"></div>
-                            </div>
-
-                            <div class="row mt-5">
-                                <div class="col-2"></div>
-                                <div class="col price-window text-right pt-2 pb-2">
-                                    &nbsp;
-                                    <transition name="slide-fade">
+                                <div class="row mt-5">
+                                    <div class="col-2"></div>
+                                    <div class="col price-window text-right pt-2 pb-2">
+                                        &nbsp;
+                                        <transition name="slide-fade">
                                         <span v-if="packType.startsWith('elite')">
                                             {{ price | toEth }} ETH
                                         </span>
-                                    </transition>
+                                        </transition>
+                                    </div>
+                                    <div class="col-2"></div>
                                 </div>
-                                <div class="col-2"></div>
-                            </div>
 
-                            <button class="btn btn-secondary mt-3" :disabled="packType.startsWith('reg')" @click="buyCard()">Purchase</button>
+                                <button class="btn btn-secondary mt-3" :disabled="packType.startsWith('reg')" @click="buyCard()">Purchase</button>
+                            </div>
                         </div>
                     </div>
+                    <div class="col-lg"></div>
                 </div>
-                <div class="col-lg"></div>
-            </div>
+            </transition>
         </div>
     </div>
 </template>
@@ -157,7 +159,7 @@
 
                 const cardsPerPack = 3;
                 this.buyState = 'mining';
-                
+
                 const num = parseInt(this.packType.split('-')[1]) * cardsPerPack;
 
                 console.log(`Buying ${this.packType} = ${num} cards`);
@@ -277,12 +279,23 @@
     .slide-fade-enter-active {
         transition: all .2s ease;
     }
+
     .slide-fade-leave-active {
         transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
     }
+
     .slide-fade-enter, .slide-fade-leave-to
-        /* .slide-fade-leave-active below version 2.1.8 */ {
+        /* .slide-fade-leave-active below version 2.1.8 */
+    {
         transform: translateX(10px);
+        opacity: 0;
+    }
+
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .75s;
+    }
+
+    .fade-enter, .fade-leave-to {
         opacity: 0;
     }
 </style>
