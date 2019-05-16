a6dd18857f397be2af5a10fb2a56365668e0cd10
James Morgan <james.morgan.e@googlemail.com>
2019-05-16 14:00:02 +0100

    Adding partners

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,44 +1,42 @@
 <template>
-    <div class="justify-content-sm-center mt-5 pt-5 pb-5 bg-light">
+    <div class="justify-content-sm-center mt-5 pt-5 pb-5 bg-secondary">
 
         <div class="row">
             <div class="col pb-4">
-                <h4>Partners</h4>
+                <page-sub-title text="Partners" variant="orange"></page-sub-title>
             </div>
         </div>
 
         <div class="row">
-            <div class="col">
+            <div class="col-6 col-sm p-4">
                 <a href="https://www.brewdog.com/" target="_blank">
                     <img src="../assets/partners/BD_Logo_Full_Smooth_Black-1492x2093-3dee766.svg" class="img-fluid"
                          style="max-height: 100px"/>
                 </a>
             </div>
-            <div class="col">
+            <div class="col-6 col-sm p-4">
                 <a href="http://opensea.io" target="_blank">
                     <img src="../assets/partners/opensea-logo-flat-colored-blue-vertical.svg" class="img-fluid"
                          style="max-height: 100px"/>
                 </a>
             </div>
-            <div class="col">
-                <div class="col">
-                    <a href="https://3box.io" target="_blank">
-                        <img src="../assets/partners/3box-logo.png" class="img-fluid primary" style="max-height: 100px"/>
-                    </a>
-                </div>
+            <div class="col-6 col-sm p-4">
+                <a href="https://3box.io" target="_blank">
+                    <img src="../assets/partners/3box-logo.png" class="img-fluid primary" style="max-height: 100px"/>
+                </a>
             </div>
-            <div class="col">
+            <div class="col-6 col-sm p-4">
                 <a href="https://www.blocknative.com" target="_blank">
                     <img src="../assets/partners/home-blocknative-logo-icon@2x.webp" class="img-fluid"
                          style="max-height: 100px"/>
                 </a>
             </div>
-            <div class="col">
+            <div class="col-6 col-sm p-4">
                 <a href="http://blockrocket.tech" target="_blank">
                     <img src="../assets/partners/BlockR_black_logo.svg" class="img-fluid" style="max-height: 100px"/>
                 </a>
             </div>
-            <div class="col">
+            <div class="col-6 col-sm p-4">
                 <a href="https://knownorigin.io" target="_blank">
                     <img src="../assets/partners/KO_Logo_path.png" class="img-fluid" style="max-height: 100px"/>
                 </a>
@@ -48,8 +46,11 @@
 </template>
 
 <script>
+    import PageSubTitle from "./PageSubTitle";
+
     export default {
         name: 'partners',
+        components: {PageSubTitle},
     };
 </script>
 
