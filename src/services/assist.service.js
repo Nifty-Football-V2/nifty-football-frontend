e2db2102e2ec064cbf8d0fc5ba98a4e4dcd936e3
Liam Aharon <liam.aharon@hotmail.com>
2019-05-17 16:26:19 +1000

    Remove redundant options param

--- src/services/assist.service.js
+++ src/services/assist.service.js
@@ -4,7 +4,7 @@ import {ASSIST_KEY, NETWORK_ID} from '../utils';
 let initializedAssist
 
 // Assist methods
-export const initializeAssist = (web3, options) => getAssist(web3, options) // Call this function as soon as web3 is initialized
+export const initializeAssist = (web3) => getAssist(web3) // Call this function as soon as web3 is initialized
 export const onboardUser = () => getAssist().onboard()
 export const decorateContract = contract => getAssist().Contract(contract)
 export const decorateTransaction = txObject => getAssist().Transaction(txObject)
@@ -25,14 +25,13 @@ export const messages = ({isElite}) => ({
 
 // Returns initialized assist object if previously initialized.
 // Otherwise will initialize assist with the config object
-export function getAssist(web3, options) {
+export function getAssist(web3) {
   if (!initializedAssist) {
     initializedAssist = bnc.init(
       {
         networkId: NETWORK_ID,
         dappId: ASSIST_KEY, // from https://accounts.blocknative.com
-        web3,
-        ...options
+        web3
       }
     )
   }
