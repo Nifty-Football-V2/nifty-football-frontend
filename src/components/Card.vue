9713b8ae19742d8cb56ac017fb72f79b7dd1a107
Andy Gray <andy@whatsthescore.co.uk>
2019-09-16 17:38:23 +0100

    click cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,10 @@
 <template>
-    <vue-flip :active-click="true" width="100%" class="card-height">
-        <div slot="front">
+    <div>
+        <div v-show="showFront" v-on:click="flipCard">
             <lazy-img-loader :src="card.tokenId" :id="card.tokenId" :lazy="lazy"></lazy-img-loader>
         </div>
-        <div slot="back">
+
+        <div v-show="!showFront" v-on:click="flipCard">
             <div v-if="card">
                 <card-back :card="card"></card-back>
             </div>
@@ -11,17 +12,21 @@
                 <img src="../assets/holding.svg" alt="loading"/>
             </div>
         </div>
-    </vue-flip>
+    </div>
 </template>
 
 <script>
-    import VueFlip from 'vue-flip';
-    import LazyImgLoader from "./LazyImgLoader";
-    import CardBack from "./CardBack";
+    import LazyImgLoader from './LazyImgLoader';
+    import CardBack from './CardBack';
 
     export default {
         name: 'card',
-        components: {CardBack, LazyImgLoader, VueFlip},
+        components: {CardBack, LazyImgLoader},
+        data() {
+            return {
+                showFront: true,
+            };
+        },
         props: {
             card: {
                 type: Object,
@@ -31,51 +36,21 @@
                 default: true
             },
         },
-        computed: {},
-        methods: {},
+        methods: {
+            flipCard() {
+                this.showFront = !this.showFront;
+            },
+        },
     };
 </script>
 
 <style lang="scss">
     @import "../colours";
 
-    // 2 cards per row
-    // Extra small devices (portrait phones, less than 576px)
-    @media (max-width: 575px) {
-        .card-height {
-            height: 21rem;
-        }
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .5s;
     }
-
-    // 4 cards per row
-    // Small devices (landscape phones, 576px and up)
-    @media (min-width: 576px) and (max-width: 767px) {
-        .card-height {
-            height: 15rem;
-        }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
     }
-
-    // 4 cards per row
-    // Medium devices (tablets, 768px and up)
-    @media (min-width: 768px) and (max-width: 991px) {
-        .card-height {
-            height: 20rem;
-        }
-    }
-
-    // 6 cards per row
-    // Large devices (desktops, 992px and up)
-    @media (min-width: 992px) and (max-width: 1199px) {
-        .card-height {
-            height: 15rem;
-        }
-    }
-
-    // Extra large devices (large desktops, 1200px and up)
-    @media (min-width: 1200px) {
-        .card-height {
-            height: 20rem;
-        }
-    }
-
 </style>
